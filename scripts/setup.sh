2ccca91b39a10bea949d759dca3846d8aa7051bf
Giovanni Toledo <cybersecgio@gmail.com>
2021-08-02 22:44:11 -0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -2,36 +2,36 @@
 
 # Script to install dependencies, set up ansible, render configuration files 
 # to run the setup:
-#   ./setup.sh 
+#   sudo ./setup.sh 
 # to run the setup AND run the playbooks 
-#   ./setup.sh install
+#   sudo ./setup.sh install
 
 
 # check for root 
-if [ $UID -ne 0 ]; then
-    echo 'Please run as root!';
-    exit;
+if [ $UID -ne 0 ] then
+    echo 'Please run as root!'
+    exit
 fi
 
 # install python3-pip
 echo '[!] installing pip3...' 
 apt update > /dev/null 2>&1
 apt install python3-pip -y > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo -e '[x] failed! \n[!] please install pip3 manually';
-    exit;
+if [ $? -ne 0 ] then
+    echo -e '[x] failed! \n[!] please install pip3 manually'
+    exit
 else
-    echo -e '[+] done\n';
+    echo -e '[+] done\n'
 fi
 
 # install jinja2
 echo '[!] installing jinja2...'
 python3 -m pip install jinja2 > /dev/null 2>&1
-if [ $? -ne 0 ]; then
-    echo '[x] failed!';
-    exit;
+if [ $? -ne 0 ] then
+    echo '[x] failed!'
+    exit
 else 
-    echo -e '[+] done\n';
+    echo -e '[+] done\n'
 fi
 
 # create rendered/
@@ -57,7 +57,7 @@ echo -e '[+] done\n'
 echo -e '\n[+] SETUP COMPLETE\n'
 
 # if ./setup.sh install 
-if [ ! -z $1 ] && [ $1 = 'install' ]; then
+if [ ! -z $1 ] && [ $1 = 'install' ] then
     echo -e '[!] Running Playbooks\n'
     cd ../Ansible/playbooks
     # install elk server first
